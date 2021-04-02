53534002a329915b27d6051b195efe1c74f1f8c0
Matthew Collier <mcollier@digitalbazaar.com>
2021-04-02 09:43:32 -0400

    Update generate-seed.

--- util/generate-seed.js
+++ util/generate-seed.js
@@ -3,9 +3,17 @@
  */
 'use strict';
 
+const {decode} = require('base58-universal');
+const {driver} = require('@digitalbazaar/did-method-key');
 const {generateId} = require('bnid');
 
+const didKeyDriver = driver();
+
 (async () => {
-  const seed = await generateId({bitLength: 256, multibase: false});
-  console.log(`SEED: ${seed}`);
+  const seedBase58 = await generateId({bitLength: 256, multibase: false});
+  console.log(`SECRET seedBase58: ${seedBase58}`);
+  // convert base58 to Uint8Array
+  const seed = decode(seedBase58);
+  const didKey = await didKeyDriver.generate({seed});
+  console.log('PUBLIC DID', didKey.didDocument.id);
 })();
