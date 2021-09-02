e675be9f40ce557815f035470fb0ce5514918083
Ganesh Annan <gannan@digitalbazaar.com>
2021-09-02 17:45:51 -0400

    Use multibase for encoding seeds.

--- test/test.config.js
+++ test/test.config.js
@@ -11,12 +11,12 @@ config.mocha.tests.push(path.join(__dirname, 'mocha'));
 config['app-identity'].seeds = {
   app: {
     id: 'did:key:z6MkwQfUzcoF4zpUPGo629xyZiQZahsZjRz5qaqMaD5GjwnQ',
-    seedBase58: 'AKXkfGfos4k2KEx5iXunVJfXmM9kzT1H6eDaVdBRHCTt'
+    seedMultibase: 'z1AfffcYWj1GaLcZt1Sz9bGgNLDm9k6q7GJaGjz36qyogLi'
   },
   services: {
     test: {
       id: 'did:key:z6MkwQfUzcoF4zpUPGo629xyZiQZahsZjRz5qaqMaD5GjwnQ',
-      seedBase58: 'AKXkfGfos4k2KEx5iXunVJfXmM9kzT1H6eDaVdBRHCTt',
+      seedMultibase: 'z1AfffcYWj1GaLcZt1Sz9bGgNLDm9k6q7GJaGjz36qyogLi',
       serviceType: 'test'
     }
   }
