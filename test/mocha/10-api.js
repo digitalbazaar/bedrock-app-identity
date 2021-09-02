ab0375195abdd1c6089b42b18d95161af7db0538
Ganesh Annan <gannan@digitalbazaar.com>
2021-09-02 15:03:21 -0400

    Update API.

--- test/mocha/10-api.js
+++ test/mocha/10-api.js
@@ -1,11 +1,27 @@
 /*
  * Copyright (c) 2020-2021 Digital Bazaar, Inc. All rights reserved.
  */
-const {APPLICATION_KEYS} = require('bedrock-app-key');
+const {getAppIdentity, getServiceIdentities} = require('bedrock-app-key');
 
 describe('bedrock-app-key API', () => {
-  it('has the proper exports', async () => {
-    const {keys} = APPLICATION_KEYS.get('app');
+  it('getAppIdentity() has the proper exports', async () => {
+    const {keys} = getAppIdentity();
+    keys.should.have.keys([
+      'authenticationKey', 'assertionMethodKey', 'capabilityDelegationKey',
+      'capabilityInvocationKey', 'keyAgreementKey'
+    ]);
+    keys.capabilityDelegationKey.signer.should.be.a('function');
+    keys.capabilityDelegationKey.type.should.equal(
+      'Ed25519VerificationKey2020');
+    keys.capabilityInvocationKey.signer.should.be.a('function');
+    keys.capabilityInvocationKey.type.should.equal(
+      'Ed25519VerificationKey2020');
+    keys.keyAgreementKey.type.should.equal('X25519KeyAgreementKey2020');
+  });
+  it('getServiceIdentities() has the proper exports', async () => {
+    const serviceIdentities = getServiceIdentities();
+    const {keys, serviceType} = serviceIdentities.get('test');
+    serviceType.should.equal('test');
     keys.should.have.keys([
       'authenticationKey', 'assertionMethodKey', 'capabilityDelegationKey',
       'capabilityInvocationKey', 'keyAgreementKey'
