2b600a5aede7d841ade9843addc0bc061c6a0934
Dave Longley <dlongley@digitalbazaar.com>
2025-09-21 17:11:30 -0400

    Use `@digitalbazaar/did-method-key@5`.

--- util/generate-seed.js
+++ util/generate-seed.js
@@ -1,15 +1,24 @@
 /*!
- * Copyright (c) 2020-2022 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2020-2025 Digital Bazaar, Inc. All rights reserved.
  */
+import * as Ed25519Multikey from '@digitalbazaar/ed25519-multikey';
+import {decodeSecretKeySeed, generateSecretKeySeed} from 'bnid';
 import {driver} from '@digitalbazaar/did-method-key';
 import {generateSecretKeySeed, decodeSecretKeySeed} from 'bnid';
 
 const didKeyDriver = driver();
+didKeyDriver.use({
+  multibaseMultikeyHeader: 'z6Mk',
+  fromMultibase: Ed25519Multikey.from
+});
 
 // generate a random seed
 const secretKeySeed = await generateSecretKeySeed();
 const seedBytes = await decodeSecretKeySeed({secretKeySeed});
-const didKey = await didKeyDriver.generate({seed: seedBytes});
 
-console.log('PUBLIC DID', didKey.didDocument.id);
+// generate `did:key` DID
+const verificationKeyPair = await Ed25519Multikey.generate({seed: seedBytes});
+const {didDocument} = await didKeyDriver.fromKeyPair({verificationKeyPair});
+
+console.log('PUBLIC DID', didDocument.id);
 console.log(`SECRET seedMultibase: ${secretKeySeed}`);
